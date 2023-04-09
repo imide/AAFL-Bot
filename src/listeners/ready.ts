e8b0ee8d3f840b69d80795811891690114ae3301
imide <43657624+imide@users.noreply.github.com>
2023-04-08 21:29:54 -0700

    a

---
+++ src/listeners/ready.ts
@@ -0,0 +1,5 @@
+import { Client } from 'discord.js';
+
+export function execute(client: Client) {
+  console.log('Bot is ready!');
+}
---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import { Client, GatewayIntentBits, REST, Routes, ClientOptions } from 'discord.js';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import { execute as ready } from './listeners/ready';
+
+dotenv.config();
+
+
+const client = new Client({
+    intents: 
+    [GatewayIntentBits.Guilds],
+});
+
+const commands: any[] = [];
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+  const { data } = require(`./commands/${file}`);
+  commands.push(data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log('Successfully registered application commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.once('ready', () => {
+  ready(client);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = interaction.commandName;
+
+  try {
+    const { execute } = require(`./commands/${command}`);
+    await execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
