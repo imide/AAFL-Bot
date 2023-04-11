9daacd7da9cf1d0a983da6d1ef0687324b6909ab
imide <43657624+imide@users.noreply.github.com>
2023-04-11 19:56:08 +0000

    added shit

--- src/commands/recruit.ts
+++ src/commands/recruit.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, Guild } from "discord.js"
 
 export const data = new SlashCommandBuilder()
     .setName('recruit')
@@ -26,10 +26,32 @@ export const data = new SlashCommandBuilder()
             { name: 'San Francisco 49ers', value: '49ers' },
             { name: 'Seattle Seahawks', value: 'seahawks' },
             { name: 'Tennessee Titans', value: 'titans' },
-        )
+        ))
+
     .addNumberOption(option =>
         option.setName('seasons')
         .setDescription('The number of seasons to recruit the player for.')
+        .setRequired(true)
+        .setAutocomplete(true)
         )
-        )
+
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    const member = interaction.options.getMember('user')!;
+    const teamName = interaction.options.getString('team')!;
+    const seasons = interaction.options.getNumber('seasons')!;
+    const guild = interaction.guild!;
+    const teamRole = guild.roles.cache.find(role => role.name === teamName)!;
+
+    if (member.roles.cache.some((role: { name: string; }) => role.name === teamRole.name)) {
+        const error1 = new EmbedBuilder()
+        .setTitle('Error')
+        .setColor('#ff0000')
+        .setDescription('This user is already on a team. Check the players roles or initiate a trade.')
+        .setTimestamp()
+        await interaction.reply({ embeds: [error1], ephemeral: true })
+    }
+
+    
+}
      
