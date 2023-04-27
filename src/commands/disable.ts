9a121a63154324f4ea63b23a61bb1a6b42171936
imide <3657624+imide@users.noreply.github.com>
2023-04-27 18:20:16 +0000

    organizing v.0.1 12:20

---
+++ src/commands/disable.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+
+
+export const data = new SlashCommandBuilder()
+    .setName("disable")
+    .setDescription("Disables the bot for whatever reason.")
+    .addBooleanOption((option) =>
+        option
+        .setName("disable")
+        .setDescription("Set to true to disable the bot. **Only use this in emergencies!**")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+
